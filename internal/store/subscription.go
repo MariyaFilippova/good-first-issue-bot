07e219aa4926a069b5e28d8015523d5445276ca2
Maria Filippova <maxa.spb6@gmail.com>
2026-07-09 21:37:07 +0200

    refactor

--- internal/store/subscription.go
+++ internal/store/subscription.go
@@ -3,16 +3,16 @@ package store
 import "context"
 
 func (s Store) Subscribe(ctx context.Context, email, owner, name string) error {
-	user_id, err := s.GetUser(ctx, email)
+	userId, err := s.GetUser(ctx, email)
 	if err != nil {
 		return err
 	}
 
-	repo_id, err := s.GetRepo(ctx, owner, name)
+	repoId, err := s.GetRepo(ctx, owner, name)
 	if err != nil {
 		return err
 	}
 
-	_, err = s.pool.Exec(ctx, `INSERT INTO subscriptions (user_id, repo_id) VALUES ($1, $2)`, user_id, repo_id)
+	_, err = s.pool.Exec(ctx, `INSERT INTO subscriptions (user_id, repo_id) VALUES ($1, $2)`, userId, repoId)
 	return err
 }
